93cbdbd566bf9eabd94de1cc7dde962637ca3930
RubenAcosta16 <1511ruben@gmail.com>
2024-10-10 17:53:57 -0600

    next ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-
-const { nextui } = require("@nextui-org/react")
-
+import { nextui } from "@nextui-org/react";
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-       "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
-    ],
-    theme: {
-      extend: {},
-    },
-    darkMode: "class",
-  plugins: [nextui()]
-  }
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+};
