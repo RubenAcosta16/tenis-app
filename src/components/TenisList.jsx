d058bcdc4873c641725874ae29626abcdeebb258
RubenAcosta16 <1511ruben@gmail.com>
2024-10-16 19:08:29 -0600

    aa

--- src/components/TenisList.jsx
+++ src/components/TenisList.jsx
@@ -13,70 +13,70 @@ const sneakers = [
     precio: 90,
     valoracion: 5.0,
     bestSeller: true, // Es un best seller
-    imagen: "/teni2.png",
+    imagen: "/teni3.png",
   },
   {
     nombre: "Nike ZoomX Vaporfly NEXT% 2",
     precio: 250,
     valoracion: 4.5,
     bestSeller: false,
-    imagen: "https://example.com/nike-zoomx-vaporfly-next2.png",
+    imagen:  "/teni3.png",
   },
   {
     nombre: "Nike React Infinity Run Flyknit",
     precio: 160,
     valoracion: 4.0,
     bestSeller: true, // Es un best seller
-    imagen: "https://example.com/nike-react-infinity-run.png",
+    imagen:  "/teni4.png",
   },
   {
     nombre: "Nike Blazer Mid '77",
     precio: 100,
     valoracion: 3.5,
     bestSeller: false,
-    imagen: "https://example.com/nike-blazer-mid77.png",
+    imagen: "/teni3.png",
   },
   {
     nombre: "Nike Air Max 90",
     precio: 130,
     valoracion: 4.5,
     bestSeller: true, // Es un best seller
-    imagen: "https://example.com/nike-air-max-90.png",
+    imagen:  "/teni2.png",
   },
   {
     nombre: "Nike Court Vision Low",
     precio: 75,
     valoracion: 4.0,
     bestSeller: false,
-    imagen: "https://example.com/nike-court-vision-low.png",
+    imagen:  "/teni4.png",
   },
   {
     nombre: "Nike SB Dunk Low",
     precio: 110,
     valoracion: 5.0,
     bestSeller: true, // Es un best seller
-    imagen: "https://example.com/nike-sb-dunk-low.png",
+    imagen:  "/teni3.png",
   },
   {
     nombre: "Nike Air Huarache",
     precio: 120,
     valoracion: 4.0,
     bestSeller: false,
-    imagen: "https://example.com/nike-air-huarache.png",
+    imagen:  "/teni3.png",
   },
-  {
+  { 
     nombre: "Nike Air Jordan 1 Retro",
     precio: 180,
     valoracion: 4.5,
     bestSeller: true, // Es un best seller
-    imagen: "https://example.com/nike-air-jordan-1-retro.png",
+    imagen:  "/teni2.png",
   },
   {
     nombre: "Nike ACG Air Mowabb",
     precio: 160,
     valoracion: 4.0,
     bestSeller: false,
-    imagen: "https://example.com/nike-acg-air-mowabb.png",
+    imagen:  "/teni4.png",
   },
 ];
 
